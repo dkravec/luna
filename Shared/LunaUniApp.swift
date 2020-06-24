f626b837937831ee6f7453211bedb497b3578cdf
dkravec <daniel@kravec.com>
2020-06-24 00:35:39 -0400

    1.0 (b13.6): June 24, 2020

---
+++ Shared/LunaUniApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaUniApp.swift
+//  Shared
+//
+//  Created by 46-1920 on 2020-06-24.
+//
+
+import SwiftUI
+
+@main
+struct LunaUniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
